77d1117375d04657df355db1e25db13a2d75d2b3
Jaaystones <Joelpaul345@gmail.com>
2023-11-10 10:33:57 +0100

    :wrench: Updated code

--- Controllers/authenticateUser.js
+++ Controllers/authenticateUser.js
@@ -2,49 +2,12 @@ const Users = require('../models/Users');
 const bcrypt = require('bcrypt');
 const asyncHandler = require('express-async-handler');
 
-
-  const verifyEmailAndToken = async (req, res) => {
-    const { email, token } = req.body; // Extract email and token from the body
-    try {
-      const user = await Users.findOne({ email }).exec();
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-
-      const decoded = await bcrypt.compare( token, user.authToken.token);
-      if (!decoded) {
-      return res.status(403).json({ message: 'Token not valid' });
-      }
-      // // Check if the token matches the stored token
-      // if (user.authToken.token !== token) {
-      //   return res.status(403).json({ message: 'Invalid token' });
-      // }
-
-
-      const currentTime = new Date();
-
-      // Check if the token has expired
-      if (user.authToken.expiry < currentTime) {
-        return res.status(403).json({ message: 'Token has expired' });
-      }
-
-      // Mark the user as verified (you can set a flag in the user model)
-      user.isActive = true;
-      await user.save();
-      return res.status(200).json({ message: 'Token has been verified' });
-
-      //res.redirect('/login'); // Redirect to the login page
-    } catch (error) {
-      console.error('Error during email and token verification:', error);
-      res.status(500).json({ message: 'Verification failed' });
-
 const verifyEmailAndToken = asyncHandler(async (req, res) => {
   try {
     const { email, token } = req.body;
 
     if (!email || !token) {
       return res.status(400).json({ message: "Email and token are required" });
-
     }
 
     const user = await Users.findOne({ email }).exec();
@@ -65,7 +28,7 @@ const verifyEmailAndToken = asyncHandler(async (req, res) => {
       return res.status(403).json({ message: "Token has expired" });
     }
 
-    // Mark the user as verified (update the isActive statuses)
+    // Mark the user as verified (update the isActive status)
     user.isActive = true;
     await user.save();
 
